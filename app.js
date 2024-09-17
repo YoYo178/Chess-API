7e5df4550fc71ee7ab4fda33aaa3ab685be761ed
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2024-09-17 19:30:15 +0530

    Remove Jade view engine

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 import express from "express";
-import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(import.meta.dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
